11f4ee0a212dfbacfbbef4600770416de551bfea
Esteban AP <estape11@gmail.com>
2021-12-31 11:27:51 -0600

    Se elimina codigo innecesario

--- amigosecreto-whatsapp.py
+++ amigosecreto-whatsapp.py
@@ -35,20 +35,7 @@ def bot():
     resp = MessagingResponse()
     msg = resp.message()
     responded = False
-    if 'quote' in incoming_msg:
-        # return a quote
-        r = requests.get('https://api.quotable.io/random')
-        if r.status_code == 200:
-            data = r.json()
-            quote = f'{data["content"]} ({data["author"]})'
-        else:
-            quote = 'I could not retrieve a quote at this time, sorry.'
-        msg.body(quote)
-        responded = True
-    if 'cat' in incoming_msg:
-        # return a cat pic
-        msg.media('https://cataas.com/cat')
-        responded = True
+    # Mensajes predefinidos
     if 'amigo' in incoming_msg:
         msg.body("Envía tu nombre (sin tildes ni disminutivos)")
         responded = True
